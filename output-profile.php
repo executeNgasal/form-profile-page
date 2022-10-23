79f175e024c716ba16031de36f9f2bdcf295a8c6
Uqierach <knowledge.oz2704@gmail.com>
2022-10-23 22:31:23 +0700

    mengubah extensi

---
+++ output-profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=
+   , initial-scale=1.0" />
+  <link rel="stylesheet" href="/css/style.css" />
+  <link rel="stylesheet" href="css/reset.css" />
+  
+  <?php
+  $fullname = $_POST['fullname'];
+  $nickname = $_POST['nickname'];
+  $email = $_POST['email'];
+  $age = $_POST['age'];
+  $role = $_POST['role'];
+  $profile = $_POST['profile'];
+  $description = $_POST['description'];
+  ?>
+  <title>Your Profile</title>
+</head>
+
+<body>
+  <!-- Header -->
+
+</body>
+
+</html>
